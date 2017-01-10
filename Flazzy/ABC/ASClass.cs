4cd3b2bdb1e5590f625eb1da57708ebdab397704
ArachisH <iKBenPeanut@gmail.com>
2017-01-09 20:31:42 -0800

    Added ASClass disassemble support.

--- Flazzy/ABC/ASClass.cs
+++ Flazzy/ABC/ASClass.cs
@@ -4,18 +4,42 @@ using Flazzy.IO;
 
 namespace Flazzy.ABC
 {
-    public class ASClass : AS3Item
+    public class ASClass : ASContainer
     {
+        public ASInstance Instance
+        {
+            get { return ABC.Instances[InstanceIndex]; }
+        }
+        internal int InstanceIndex { get; set; }
+
+        public ASMethod Constructor
+        {
+            get { return ABC.Methods[ConstructorIndex]; }
+        }
+        public int ConstructorIndex { get; set; }
+
+        protected override string DebuggerDisplay
+        {
+            get
+            {
+                return Instance.ToAS3();
+            }
+        }
+
         public ASClass(ABCFile abc)
             : base(abc)
+        { }
+        public ASClass(ABCFile abc, FlashReader input)
+            : base(abc)
         {
+            ConstructorIndex = input.ReadInt30();
+            PopulateTraits(input);
         }
 
         public override string ToAS3()
         {
             throw new NotImplementedException();
         }
-
         public override void WriteTo(FlashWriter output)
         {
             throw new NotImplementedException();
