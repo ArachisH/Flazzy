87c3a141a473a1f790e4716b2058b88876a3dc5c
ArachisH <iKBenPeanut@gmail.com>
2017-01-09 20:35:47 -0800

    Wrap declaration of variable 'abc' in using statement.

--- Flazzy.Example/Program.cs
+++ Flazzy.Example/Program.cs
@@ -63,26 +63,29 @@ namespace Flazzy.Example
                     if (tag.Kind != TagKind.DoABC) continue;
 
                     var doAbcTag = (DoABCTag)tag;
-                    var abc = new ABCFile(doAbcTag.ABCData);
-                    ASConstantPool pool = abc.Pool;
+                    using (var abc = new ABCFile(doAbcTag.ABCData))
+                    {
+                        ASConstantPool pool = abc.Pool;
 
-                    Console.WriteLine("------------------------------");
-                    Console.WriteLine($"{doAbcTag.Name}.abc({SizeSuffix(doAbcTag.ABCData.Length)})");
-                    Console.WriteLine();
-                    Console.WriteLine($"  * Methods: {abc.Methods.Count:n0}");
-                    Console.WriteLine($"  * Metadata: {abc.Metadata.Count:n0}");
-                    Console.WriteLine($"  * Classes/Instances: {abc.Instances.Count:n0}");
-                    Console.WriteLine($"  * Scripts: {abc.Scripts.Count:n0}");
-                    Console.WriteLine($"  * Method Bodies: {abc.MethodBodies.Count:n0}");
-                    Console.WriteLine();
-                    Console.WriteLine($"  ~ Constant Pool");
-                    Console.WriteLine($"       | Integers: {pool.Integers.Count:n0}");
-                    Console.WriteLine($"       | UIntegers: {pool.UIntegers.Count:n0}");
-                    Console.WriteLine($"       | Doubles: {pool.Doubles.Count:n0}");
-                    Console.WriteLine($"       | Strings: {pool.Strings.Count:n0}");
-                    Console.WriteLine($"       | Namespaces: {pool.Namespaces.Count:n0}");
-                    Console.WriteLine($"       | NamespaceSets: {pool.NamespaceSets.Count:n0}");
-                    Console.WriteLine($"       | Multinames: {pool.Multinames.Count:n0}");
+                        Console.WriteLine("------------------------------");
+                        Console.WriteLine($"{doAbcTag.Name}.abc({SizeSuffix(doAbcTag.ABCData.Length)})");
+                        Console.WriteLine();
+                        Console.WriteLine($"  * Methods: {abc.Methods.Count:n0}");
+                        Console.WriteLine($"  * Metadata: {abc.Metadata.Count:n0}");
+                        Console.WriteLine($"  * Classes/Instances: {abc.Instances.Count:n0}");
+                        Console.WriteLine($"  * Scripts: {abc.Scripts.Count:n0}");
+                        Console.WriteLine($"  * Method Bodies: {abc.MethodBodies.Count:n0}");
+
+                        Console.WriteLine();
+                        Console.WriteLine($"  ~ Constant Pool");
+                        Console.WriteLine($"       | Integers: {pool.Integers.Count:n0}");
+                        Console.WriteLine($"       | UIntegers: {pool.UIntegers.Count:n0}");
+                        Console.WriteLine($"       | Doubles: {pool.Doubles.Count:n0}");
+                        Console.WriteLine($"       | Strings: {pool.Strings.Count:n0}");
+                        Console.WriteLine($"       | Namespaces: {pool.Namespaces.Count:n0}");
+                        Console.WriteLine($"       | NamespaceSets: {pool.NamespaceSets.Count:n0}");
+                        Console.WriteLine($"       | Multinames: {pool.Multinames.Count:n0}");
+                    }
                 }
             }
 
