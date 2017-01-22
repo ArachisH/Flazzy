6a1e73c7642e57002cd39582205acc20b45a7a0a
ArachisH <iKBenPeanut@gmail.com>
2017-01-22 00:23:50 -0800

    Dynamic keyword saves the day.

--- Flazzy/ABC/AVM2/Instructions/Arithmetic/EqualsIns.cs
+++ Flazzy/ABC/AVM2/Instructions/Arithmetic/EqualsIns.cs
@@ -1,20 +1,14 @@
-﻿using System;
-
-namespace Flazzy.ABC.AVM2.Instructions
+﻿namespace Flazzy.ABC.AVM2.Instructions
 {
-    public class EqualsIns : ArithmeticInstruction
+    public class EqualsIns : Computation
     {
         public EqualsIns()
             : base(OPCode.Equals)
         { }
 
-        protected override object Execute(object left, object right)
+        protected override object Execute(dynamic left, dynamic right)
         {
-            var cLeft = (left as IComparable);
-            var cRight = (right as IComparable);
-            if (cLeft == null || cRight == null) return null;
-
-            return (cLeft.CompareTo(cRight) == 0);
+            return (left == right);
         }
     }
 }
