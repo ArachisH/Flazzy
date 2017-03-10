573bbf62af2a0437623c53e0f336465b31c98cc3
ArachisH <iKBenPeanut@gmail.com>
2017-03-10 13:51:22 -0800

    Updated assembly info files.

--- Flazzy/Properties/AssemblyInfo.cs
+++ Flazzy/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Flazzy")]
-[assembly: AssemblyCopyright("Copyright © HuitronAJ 2017")]
+[assembly: AssemblyCopyright("Copyright © ArachisH 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
