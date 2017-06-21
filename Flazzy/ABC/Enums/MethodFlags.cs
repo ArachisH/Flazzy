148efa91922942172e72533f1ca02586245600fd
ArachisH <iKBenPeanut@gmail.com>
2017-06-21 00:51:45 -0700

    Added IgnoreRest flag Added Explicit flag Use decimal values, easier to read

--- Flazzy/ABC/Enums/MethodFlags.cs
+++ Flazzy/ABC/Enums/MethodFlags.cs
@@ -5,12 +5,14 @@ namespace Flazzy.ABC
     [Flags]
     public enum MethodFlags
     {
-        None = 0x00,
-        NeedArguments = 0x01,
-        NeedActivation = 0x02,
-        NeedRest = 0x04,
-        HasOptional = 0x08,
-        SetDxns = 0x40,
-        HasParamNames = 0x80
+        None = 0,
+        NeedArguments = 1,
+        NeedActivation = 2,
+        NeedRest = 4,
+        HasOptional = 8,
+        IgnoreRest = 16,
+        Explicit = 32,
+        SetDxns = 64,
+        HasParamNames = 128
     }
 }
