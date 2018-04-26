6937dc5e4c7f7d4e78d8d0afa372817a7eb248ba
ArachisH <iKBenPeanut@gmail.com>
2018-04-25 17:28:02 -0700

    Added support for assembling/disassembling Metadata tags

---
+++ Flazzy/Tags/MetadataTag.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+using Flazzy.IO;
+using Flazzy.Records;
+
+namespace Flazzy.Tags
+{
+    public class MetadataTag : TagItem
+    {
+        public string Metadata { get; set; }
+
+        public MetadataTag()
+            : base(TagKind.Metadata)
+        { }
+        public MetadataTag(HeaderRecord header, FlashReader input)
+            : base(header)
+        {
+            Metadata = input.ReadNullString();
+        }
+
+        public override int GetBodySize() => (Encoding.UTF8.GetByteCount(Metadata) + 1);
+        protected override void WriteBodyTo(FlashWriter output)
+        {
+            output.WriteNullString(Metadata);
+        }
+    }
+}
