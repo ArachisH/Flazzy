3bafb80c80e2d4d0a810c5195328ea9d43c4dd8a
ArachisH <iKBenPeanut@gmail.com>
2019-08-03 11:36:53 -0700

    * Added some new instructions, cool

---
+++ Flazzy/ABC/AVM2/Instructions/CallMethodIns.cs
@@ -0,0 +1,55 @@
+﻿using Flazzy.IO;
+
+namespace Flazzy.ABC.AVM2.Instructions
+{
+    public class CallMethodIns : ASInstruction
+    {
+        public int MethodIndex { get; set; }
+        public int ArgCount { get; set; }
+
+        public CallMethodIns(ABCFile abc)
+            : base(OPCode.CallMethod, abc)
+        { }
+        public CallMethodIns(ABCFile abc, FlashReader input)
+            : this(abc)
+        {
+            MethodIndex = input.ReadInt30();
+            ArgCount = input.ReadInt30();
+        }
+        public CallMethodIns(ABCFile abc, int methodIndex)
+            : this(abc)
+        {
+            MethodIndex = methodIndex;
+        }
+        public CallMethodIns(ABCFile abc, int methodIndex, int argCount)
+            : this(abc)
+        {
+            MethodIndex = methodIndex;
+            ArgCount = argCount;
+        }
+
+        public override int GetPopCount()
+        {
+            return ArgCount + 1;
+        }
+        public override int GetPushCount()
+        {
+            return 1;
+        }
+        public override void Execute(ASMachine machine)
+        {
+            for (int i = 0; i < ArgCount; i++)
+            {
+                machine.Values.Pop();
+            }
+            machine.Values.Pop(); // Receiver
+            machine.Values.Push(null);
+        }
+
+        protected override void WriteValuesTo(FlashWriter output)
+        {
+            output.WriteInt30(MethodIndex);
+            output.WriteInt30(ArgCount);
+        }
+    }
+}
---
+++ Flazzy/ABC/AVM2/Instructions/DxnsIns.cs
@@ -0,0 +1,19 @@
+﻿using Flazzy.IO;
+
+namespace Flazzy.ABC.AVM2.Instructions
+{
+    public class DxnsIns : ASInstruction
+    {
+        public int UriIndex { get; set; }
+        public string Uri => ABC.Pool.Strings[UriIndex];
+
+        public DxnsIns(ABCFile abc)
+            : base(OPCode.Dxns, abc)
+        { }
+        public DxnsIns(ABCFile abc, FlashReader input)
+            : this(abc)
+        {
+            UriIndex = input.ReadInt30();
+        }
+    }
+}
