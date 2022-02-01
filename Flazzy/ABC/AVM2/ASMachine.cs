764dba1c15df5d7deb41848ae2f7ec1d8c1ef035
ArachisH <iKBenPeanut@gmail.com>
2022-01-31 17:48:27 -0800

    ASCode object reference not required Set initial capacity of the Registers property to the localCount given

--- Flazzy/ABC/AVM2/ASMachine.cs
+++ Flazzy/ABC/AVM2/ASMachine.cs
@@ -1,27 +1,16 @@
-﻿using System.Collections.Generic;
-
-namespace Flazzy.ABC.AVM2
+﻿namespace Flazzy.ABC.AVM2
 {
     public class ASMachine
     {
-        private readonly ASCode _code;
-
         public Stack<object> Values { get; }
         public Stack<object> Scopes { get; }
         public Dictionary<int, object> Registers { get; }
 
-        public ASMachine(ASCode code, int localCount)
+        public ASMachine(int localCount)
         {
-            _code = code;
-
             Values = new Stack<object>();
             Scopes = new Stack<object>();
-
-            Registers = new Dictionary<int, object>();
-            for (int i = 0; i < localCount; i++)
-            {
-                Registers[i] = null;
-            }
+            Registers = new Dictionary<int, object>(localCount);
         }
     }
 }
